79c30bd770425909a883bd4490b7d2134e22f95f
sms7182 <sms7182@gmail.com>
2019-04-12 13:42:54 +0430

    first commit for cms

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine','handlebars');
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
