35e434864ea3785f18ad9940fee3fc6d529923c5
sms7182 <sms7182@gmail.com>
2019-04-19 15:14:45 +0430

    read and save data

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const  app=express();
 const  path=require('path');
 const mongoose=require('mongoose');
 const exphbs = require('express-handlebars');
+const bodyParser=require('body-parser');
+
+mongoose.Promise=global.Promise;
+
 
 mongoose.connect('mongodb://localhost:27017/cms',{useMongoClient:true}).then((db)=>{
    console.log('mongo connected ')
@@ -15,6 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
+
+//body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 //load routes
 const  home=require('./routes/home/index');
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,13 +1,19 @@
 const  express=require('express');
-
 const router=express.Router();
+const Post=require('../../models/Post');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout='admin';
     next();
 });
 router.get('/',(req,res)=>{
- res.send('it work');
+
+    Post.find({}).then(posts=>{
+        res.render('admin/posts',{posts:posts});
+    }).catch(err=>{
+        console.log('could not to loading ..');
+    });
+
 });
 
 router.get('/create',(req,res)=>{
@@ -15,6 +21,22 @@ router.get('/create',(req,res)=>{
 })
 
 router.post('/create',(req,res)=>{
-  res.send('worked');
+   let allowComments=false;
+   if(req.body.allowComments){
+       allowComments=true;
+   }
+    const  newPost=new Post({
+       title:req.body.title,
+       status:req.body.status,
+       allowComments:allowComments,
+       body:req.body.body
+   });
+   newPost.save().then(savedPost=>{
+       console.log(savedPost);
+       res.redirect('/admin/posts');
+   }).catch(error=>{
+       console.log('could not  save');
+   });
+
 })
 module.exports=router;
