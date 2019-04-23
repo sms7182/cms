8d0023055bc086430103d19a6065a23dfca59da1
sms7182 <sms7182@gmail.com>
2019-04-23 22:10:02 +0430

    dummi save

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,8 @@
 const  express=require('express');
-
+const faker=require('faker');
 const router=express.Router();
+const User=require('../../models/Post');
+
 
 router.all('/*',(req,res,next)=>{
  req.app.locals.layout='admin';
@@ -16,6 +18,25 @@ router.get('/',(req,res)=>{
 router.get('/dashboard',(req,res)=>{
 
     res.render('admin/dashboard');
+});
+
+router.post('/generate-fake-posts',(req,res)=>{
+
+    for(let i=0;i<req.body.amount;i++){
+        let post=new User();
+        post.title=faker.name.title();
+        post.status='private';
+        post.allowComments=faker.random.boolean();
+        post.body=faker.lorem.sentence();
+
+        post.save(function (err) {
+            if(err){
+                throw err;
+            }
+        });
+
+    }
+    res.redirect('/admin/posts');
 });
 
 module.exports=router;
